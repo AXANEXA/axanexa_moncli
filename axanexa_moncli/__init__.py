25552c4c7d440119775e5ce2ed5029fa3722c1c9
Triet Phan <tphan@axanexa.com>
2023-11-17 11:10:58 -0600

    Config

--- axanexa_moncli/__init__.py
+++ axanexa_moncli/__init__.py
@@ -1,8 +1,11 @@
+import importlib
 
 from .enums import *
 from .config import *
 from .error import *
 from . import api_v2 as api
 from . import entities as en, column_value as cv
+
+api = importlib.import_module(f".api_{api_version}", package="moncli")
 
 client = en.MondayClient()
--- axanexa_moncli/config.py
+++ axanexa_moncli/config.py
@@ -1,4 +1,10 @@
+# API version
+DEFAULT_API_VERSION = "v2"
+api_version = DEFAULT_API_VERSION
+
+
 # Supported date formats.
 DATE_FORMAT = '%Y-%m-%d'
 TIME_FORMAT = '%H:%M:%S'
 ZULU_FORMAT = '{}T{}.%fZ'
+
